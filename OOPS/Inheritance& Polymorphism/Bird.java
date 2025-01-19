330757432817be8891fa93f0785ff52cfb7bd5d1
Kavya B <108563555+kavyaa171@users.noreply.github.com>
2025-01-20 03:04:26 +0530

    Create Bird.java

---
+++ OOPS/Inheritance& Polymorphism/Bird.java
@@ -0,0 +1,15 @@
+public class Bird{
+    public String name;
+    public String beak;
+
+    public void fly(){
+        System.out.print("Birds are Flying");
+    }
+    public Bird(String name, String beak){
+        this.name = name;
+        this.beak = beak;
+    }
+    public Bird(){
+
+    }
+}
